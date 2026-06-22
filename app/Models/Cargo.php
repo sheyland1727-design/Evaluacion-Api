4bec735ee46e02c46dd8d7cc8c4f24b3128eb71c
Sheyland <sheyland1727@gmail.com>
2026-06-22 09:37:28 -0500

    Configuracion del archivo .env, creacion de la base de datos y creacion de los modelos y las migraciones

---
+++ app/Models/Cargo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cargo extends Model
+{
+    /** @use HasFactory<\Database\Factories\CargoFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2026_06_22_141711_create_funcion_cargos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+    Schema::create('funcion_cargos', function (Blueprint $table) {
+        $table->id();
+
+        $table->foreignId('cargo_id')
+            ->constrained('cargos')
+            ->cascadeOnDelete();
+
+        $table->text('descripcion_funcion');
+        $table->boolean('estado')->default(true);
+
+        $table->timestamps();
+    });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('funcion_cargos');
+    }
+};
