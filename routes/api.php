78fbd62f155978a035233b319246ad4f9fd5211c
Sheyland <sheyland1727@gmail.com>
2026-06-22 20:42:33 -0500

    CRUD de funciones de cargo

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CargoController;
 use App\Http\Controllers\Api\EmpleadoController;
+use App\Http\Controllers\Api\FuncionCargoController;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -23,5 +24,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/empleados', [EmpleadoController::class, 'store']);
     Route::put('/empleados/{id}', [EmpleadoController::class, 'update']);
     Route::delete('/empleados/{id}', [EmpleadoController::class, 'destroy']);
+
+    // CRUD Funciones
+Route::get('/funciones', [FuncionCargoController::class, 'index']);
+Route::get('/funciones/{id}', [FuncionCargoController::class, 'show']);
+Route::post('/funciones', [FuncionCargoController::class, 'store']);
+Route::put('/funciones/{id}', [FuncionCargoController::class, 'update']);
+Route::delete('/funciones/{id}', [FuncionCargoController::class, 'destroy']);
 
 });
